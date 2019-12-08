4fcd965331da0a6a0771c9845c6c38793d252d24
saurabhg31 <saurabhghosh31@gmail.com>
2019-12-08 14:55:36 +0530

    Added title & removed unneeded spaces

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+        <title>{{config('app.name', 'Laravel')}}</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
         <!-- Styles -->
         <style>
             html, body {
@@ -20,35 +17,28 @@
                 margin: 0;
                 background: url('{{asset('images/nature.jpg')}}')
             }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -58,7 +48,6 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -82,7 +71,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{env('APP_NAME', 'Laravel')}}
+                    {{config('app.name', 'Laravel')}}
                     <br>
                     <span style="font-size: 20px;">&copy;&nbsp;&nbsp;&nbsp;Laravel</span>
                 </div>
