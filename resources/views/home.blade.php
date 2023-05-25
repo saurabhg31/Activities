1875677194c7b98005748c562946f3448efafb39
Saurabh Ghosh <saurabh.ghosh@nichi.com>
2023-05-25 16:03:18 +0530

    Js minified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,5 +59,5 @@
     </div>
 @endsection
 @section('scripts')
-    <script type="text/javascript" src="{{ asset('js/custom/dashboard.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/custom/dashboard.min.js') }}"></script>
 @endsection
