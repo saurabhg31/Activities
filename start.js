a42b75088287032d59a176f0340c9ee366870353
saurabhg31 <saurabhghosh31@gmail.com>
2024-03-07 05:02:57 +0530

    start script optimizations

--- start.js
+++ start.js
@@ -1,5 +1,13 @@
 const LOG_ERRORS_ONLY = false;
+const CHECK_MYSQL_SERVICE = true;
 const { exec } = require('child_process');
+
+checkMySqlServiceStatus();
+executeCommand('php artisan serve');
+executeCommand('php artisan queue:work');
+executeCommand('php artisan schedule:work');
+
+console.log('\n Server started at: http://localhost:8000/ \n');
 
 function executeCommand(command) {
   exec(command.toString(), (error, stdout, stderr) => {
@@ -18,9 +26,16 @@ function executeCommand(command) {
   });
 }
 
-executeCommand('service mysql start');
-executeCommand('php artisan serve');
-executeCommand('php artisan queue:work');
-executeCommand('php artisan schedule:work');
-
-console.log('Server started at: http://localhost:8000/');
+async function checkMySqlServiceStatus() {
+  if (CHECK_MYSQL_SERVICE) {
+    console.log('Checking mysql service ... ');
+    exec('mysql', (error, stdout, stderr) => {
+      if (stderr.includes("Can't connect to local MySQL server through socket")) {
+        console.log('MySQL service inactive, restarting (may require `sudo` access) ... ');
+        executeCommand('service mysql start');
+      } else {
+        console.log('DONE.\n');
+      }
+    });
+  }
+}
