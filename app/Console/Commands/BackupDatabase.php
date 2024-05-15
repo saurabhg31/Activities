ea7171603f677b5093113f542c5a23496db33612
Saurabh Ghosh <saurabhghosh31@gmail.com>
2024-05-15 10:52:30 +0530

    WIP

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -3,10 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Date;
 use App\Traits\Miscellaneous;
-use Illuminate\Support\Arr;
 
 class BackupDatabase extends Command
 {
@@ -38,9 +36,8 @@ class BackupDatabase extends Command
         self::checkRequiremmments();
         die();
 
-        // declaring backup storage directory & filename
-        $storageDirectory = storage_path('Database Dumps/');
-        $databaseBackupFileName = storage_path('Database Dumps/DatabaseBackup_' . Date::now()->format('Y_m_d_H_i'));
+        // declaring backup filename
+        $databaseBackupFileName = storage_path(getenv('DB_BACKUP_STORAGE_FOLDER') . '/DatabaseBackup_' . Date::now()->format('Y_m_d_H_i'));
 
         // tables attributes for backup
         $tableProperties = [
@@ -54,11 +51,6 @@ class BackupDatabase extends Command
         // generating command for backing up database tables that don't require compression
         $shellCommand = 'mysqldump -u ' . getenv('DB_USERNAME') . ' -p ' . getenv('DB_DATABASE') . ' ' . implode(' ', $tableProperties['tablesWithoutCompressionRequirement']) . ' > ' . '"' . $databaseBackupFileName . '.sql"';
 
-        // creating directory with read & write access only  if it is absent
-        if (!file_exists($storageDirectory)) {
-            mkdir($storageDirectory, 0700);
-        }
-
         // executing command
         print('Backing up tables ... ' . PHP_EOL);
         if ($this->executeCommand($shellCommand)) {
@@ -71,7 +63,7 @@ class BackupDatabase extends Command
                 foreach ($tableProperties['compressionRequired'] as $index => $tableName) {
                     print('    ' . ($index + 1) . '. ' . $tableName . ' ... ');
                     if (self::compressTable($tableName)) {
-                        $this->removeLastLine();
+                        // TODO: Add post compression action code
                     } else {
                         print(PHP_EOL . 'Compression of table "' . $tableName . '" failed!' . PHP_EOL);
                     }
@@ -121,12 +113,37 @@ class BackupDatabase extends Command
 
         // checking available free memory (including swap)
         $minimumMemoryRequired = [
-            'normal' => 1 * pow(2, 20), // in KB
+            'normal' => 0.75 * pow(2, 20), // in KB
             'swap' => 0.25 * pow(2, 20) // in KB
         ];
         if (!$this->minimumFreeMemoryIsAvailable($minimumMemoryRequired)) {
-            die('        . Insufficient free memory for completing backup process! Process aborted. E - 03.' . PHP_EOL);
+            die('Insufficient free memory for completing backup process! Process aborted. E - 03.' . PHP_EOL);
+        } else {
+            print('        . Sufficient memory available.' . PHP_EOL);
         }
-        die(PHP_EOL . 'END' . PHP_EOL);
+
+        // creating directory with read & write access only  if it is absent
+        $storageDirectory = storage_path(getenv('DB_BACKUP_STORAGE_FOLDER') . '/');
+        if (!file_exists($storageDirectory)) {
+            mkdir($storageDirectory, 0700);
+        } else {
+            // if directory is present, checking if it is empty
+            if (count(scandir($storageDirectory)) > 2) {
+                $choices = function ($response) {
+                    return in_array(strtoupper($response), ['Y', 'N', 'YES', 'NO']);
+                };
+                $choice = $this->readInputFromCli(1, ['        . Database backup directory not empty. Clear all files within the same to proceed? (Y/N): '], $choices, null, true);
+                $choice = strtoupper(reset($choice));
+                if (in_array($choice, ['Y', 'YES'])) {
+                    // clearing backup storage directory
+                    exec('rm -rf "' . $storageDirectory . '"');
+                    mkdir($storageDirectory, 0700);
+                    print('        . Database backup storage directory cleared.' . PHP_EOL);
+                } else {
+                    die('Process aborted by user.' . PHP_EOL);
+                }
+            }
+        }
+        print('        . Database backup storage directory is active.' . PHP_EOL);
     }
 }
