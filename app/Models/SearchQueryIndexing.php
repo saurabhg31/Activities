38d981145b0cd07e778eedeca94fd84187e750db
saurabhg31 <saurabhghosh31@gmail.com>
2026-05-12 09:18:30 +0530

    Removed obsolete use statement

--- app/Models/SearchQueryIndexing.php
+++ app/Models/SearchQueryIndexing.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Session;
 
 class SearchQueryIndexing extends Model
 {
