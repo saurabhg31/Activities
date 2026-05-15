11787c994c2882c5cb03425779501010874876e8
saurabhg31 <saurabhghosh31@gmail.com>
2026-05-15 20:51:35 +0530

    Removed lastSearchCount from images table & model

--- app/Models/Images.php
+++ app/Models/Images.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Str;
 class Images extends Model
 {
     protected $table = 'images';
-    protected $fillable = ['type', 'image', 'imageType', 'tags', 'user_id', 'lastSearchCount', 'length', 'isAnimated'];
+    protected $fillable = ['type', 'image', 'imageType', 'tags', 'user_id', 'length', 'isAnimated'];
 
     /**
      * get images
