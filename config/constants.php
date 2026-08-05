c8ecec3ca67c460ce16e826b93a40d094557fd69
Saurabh Ghosh <saurabhghosh31@gmail.com>
2026-08-05 16:33:37 +0530

    Fine tuned hamming distance threshold, DUPLICATE_IMG_SEARCH_THRESHOLD: 15 -> 7, added comment

--- config/constants.php
+++ config/constants.php
@@ -11,5 +11,6 @@ return [
     'MAX_IMG_SIZE' => 16 * 1024 * 1024, // 16 MB. Value is in bytes.
     'TMP_STORED_PREFIX' => '<fileStoredInTempDir>',
     'SQL_MAX_BIGINT_VAL' => '18446744073709551615',
-    'DUPLICATE_IMG_SEARCH_THRESHOLD' => 15
+    // image hamming distance threshold for duplicate image detection
+    'DUPLICATE_IMG_SEARCH_THRESHOLD' => 7,
 ];
