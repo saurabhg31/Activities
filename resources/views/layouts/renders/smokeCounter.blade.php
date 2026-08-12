2c87230588918c8bbe537885a53cf9de71aa6930
Saurabh Ghosh <saurabhghosh31@gmail.com>
2026-08-12 12:13:27 +0530

    Typo fix

--- resources/views/layouts/renders/smokeCounter.blade.php
+++ resources/views/layouts/renders/smokeCounter.blade.php
@@ -19,7 +19,7 @@
     </form>
 </div>
 <div class="form-group">
-    <legend>History / Day (T: {{ number_format($data['totalCount']) }}, F/D: @if(is_null($data['frequency'])) N/A @else 1 / {{ $data['frequency'] }} @endif, PDC: {{ number_format($data['previousDatCount']) }}, DBL2C: {{ $data['dbl2c'] }}, T: <span style="color: {{$data['trend']['color']}};">{{$data['trend']['status']}}</span>)
+    <legend>History / Day (T: {{ number_format($data['totalCount']) }}, F/D: @if(is_null($data['frequency'])) N/A @else 1 / {{ $data['frequency'] }} @endif, PDC: {{ number_format($data['previousDatCount']) }}, DBL2C: {{ $data['dbl2c'] }}, TR: <span style="color: {{$data['trend']['color']}};">{{$data['trend']['status']}}</span>)
     </legend>
     <table class="table table-bordered table-striped" id="smokingCounterTable">
         <thead>
